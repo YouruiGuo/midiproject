68b16c3661fd7a09d53665449a7b587c24c6a5d7
donkeyoyo <donkeyoyo@gmail.com>
2018-11-28 11:18:59 -0700

    set bpm done

--- js/parseMIDIFile.js
+++ js/parseMIDIFile.js
@@ -1,4 +1,20 @@
 
-function parseFile(file) {
-  var fs = require('fs');
+function FileSelected(e) {
+  var files = e.target.files;
+	if (files.length > 0){
+		var file = files[0];
+		parseFile(file);
+	}
+}
+
+function parseFile(file){
+			//read the file
+	var reader = new FileReader();
+	reader.onload = function(e){
+		var partsData = MidiConvert.parse(e.target.result);
+		var mididata = JSON.stringify(partsData, undefined, 2);
+    bpm = partsData.header.bpm;
+    //console.log(bpm);
+	};
+	reader.readAsBinaryString(file);
 }
